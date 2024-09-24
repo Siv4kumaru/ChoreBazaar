5140242761ced9b7dd6a0669afe3f5452926d259
siv4kumar <21f3001256@ds.study.iitm.ac.in>
2024-09-24 11:57:49 +0530

    api full done

--- Sauce.py
+++ Sauce.py
@@ -1,11 +1,11 @@
 from flask_restful import marshal_with,Resource,Api,fields,reqparse
 from flask_security import auth_required,roles_accepted
-from models import Service,ServiceRequest
+from models import User,Service,ServiceRequest,Customer,Professional
 from extensions import db
+
 
 api=Api(prefix='/api')
 #marshelling
-
 
 parser  = reqparse.RequestParser() #convert data to json
 parser.add_argument('name',type=str,required=True)
@@ -14,7 +14,7 @@ parser.add_argument('price',type=float,required=True)
 
 #del is common for all
 delParser=reqparse.RequestParser()
-delParser.add_argument('id',type=str,required=True)
+delParser.add_argument('id',type=int,required=True)
 
 patchParser=reqparse.RequestParser()
 patchParser.add_argument('id',type=int,required=True)
@@ -54,6 +54,9 @@ reqPatchparser.add_argument('customerId', type=int)
 reqPatchparser.add_argument('professionalId', type=int)
 reqPatchparser.add_argument('serviceId', type=int)
 reqPatchparser.add_argument('serviceStatus', type=str)
+reqPatchparser.add_argument('feedback', type=str)
+reqPatchparser.add_argument('dateofcompletion', type=str)
+reqPatchparser.add_argument('dateofrequest', type=str)
 
 reqDelparser = reqparse.RequestParser()
 
@@ -67,11 +70,19 @@ class requestSauce(Resource):
         return allRequests
     
     @auth_required('token')
-    @roles_accepted('customer')
+    #@roles_accepted('customer')
     def post(self):
         args=reqPostparser.parse_args()
         if ServiceRequest.query.filter_by(customerId=args['customerId']).first() is not None and ServiceRequest.query.filter_by(professionalId=args['professionalId']).first() is not None and ServiceRequest.query.filter_by(serviceId=args['serviceId']).first() is not None:
-            return {"message":"Request Already Sent"},400
+            return {"message":"Request Already exists"},400
+        if args.get('customerId') and Customer.query.filter_by(id=args['customerId']).first() is None:
+            return {"message": "Customer does not exist"}, 400
+
+        if args.get('professionalId') and Professional.query.filter_by(id=args.get('professionalId')).first() is None:
+            return {"message": "Professional does not exist"}, 400
+
+        if args.get('serviceId') and Service.query.filter_by(id=args.get('serviceId')).first() is None:
+            return {"message": "Service does not exist"}, 400
         request=ServiceRequest(**args)
         db.session.add(request)
         db.session.commit()
@@ -91,22 +102,43 @@ class requestSauce(Resource):
     @auth_required('token')
     @roles_accepted('admin')
     def patch(self):
-        args=patchParser.parse_args()
+        args=reqPatchparser.parse_args()
         request=ServiceRequest.query.filter_by(id=args['id']).first()
+
         if request is None:
             return {"message":"Request not found"},404
-        if ServiceRequest.query.filter_by(id=args['id']).first() is not None:
-            return {"message":"Request already exists"},400
-        if args.get('name'):
-            request.name = args['name']
+        if args.get('customerId') and Customer.query.filter_by(id=args['customerId']).first() is None:
+            return {"message": "Customer does not exist"}, 400
 
-        if args.get('description'):
-            request.description = args['description']
+        if args.get('professionalId') and Professional.query.filter_by(id=args.get('professionalId')).first() is None:
+            return {"message": "Professional does not exist"}, 400
 
-        if args.get('price') is not None:
-            request.price = args['price']
+        if args.get('serviceId') and Service.query.filter_by(id=args.get('serviceId')).first() is None:
+            return {"message": "Service does not exist"}, 400
+
+        if args.get('customerId'):
+            request.customerId = args['customerId']
+
+        if args.get('professionalId'):
+            request.professionalId = args['professionalId']
+
+        if args.get('serviceId'):
+            request.serviceId = args['serviceId']
+        
+        if args.get('serviceStatus'):
+            request.serviceStatus = args['serviceStatus']
+
+        if args.get('feedback'):
+            request.feedback = args['feedback']
+
+        if args.get('dateofrequest'):
+            request.dateofrequest = args['dateofrequest']
+        
+        if args.get('dateofcompletion'):
+            request.dateofcompletion = args['dateofcompletion']
+
         db.session.commit()
-        return {"message":"Request Updated"},200
+        return {"message":f"Request Updated"},200
     
 
 
@@ -117,6 +149,7 @@ class ServiceSauce(Resource):
     def get(self):
         allServices=Service.query.all()
         return allServices
+
     
     @auth_required('token')
     @roles_accepted('admin')
