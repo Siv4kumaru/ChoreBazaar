313913e1612211e9cca46538e9b1f2eb025d1258
siv4kumaru <sivakumar@Potato.localdomain>
2024-10-31 08:31:39 +0530

    new start

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,4 @@
-broker_url = "redis://localhost:6379/1"
-result_backend = "redis://localhost:6379/2"
+broker_url = "redis://localhost:6379/0"
+result_backend = "redis://localhost:6379/1"
 timezone = "Asia/Kolkata"
 broker_connection_retry_on_startup = True
